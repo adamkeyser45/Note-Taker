44f88165f6624c23661389c94415785cd9b69572
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-08-16 12:52:30 -0500

    added working get and post routes

--- routes/apiRoutes/notesApiRoutes.js
+++ routes/apiRoutes/notesApiRoutes.js
@@ -1,18 +1,35 @@
+const fs = require('fs');
+const uniqid = require('uniqid');
 const router = require('express').Router();
+const notes = require('../../db/db.json');
 
-router.get('/api/notes', (req, res) => {
+// router.use(require('./notesApiRoutes'));
 
-    // should read the db.json file and return all saved notes as JSON.
-
+router.get('/notes', (req, res) => {
+    let data = notes;
+    res.json(data);  
 });
   
-router.post('/api/notes', (req, res) => {
-    
+router.post('/notes', (req, res) => {
     // Should receive a new note to save on the request body, add it to the db.json file, 
     // and then return the new note to the client. 
     // You'll need to find a way to give each note a unique id when it's saved 
     // (look into npm packages that could do this for you).
+    const newNote = req.body;
 
+    newNote.id = uniqid();
+
+    let data = notes;
+  
+    console.log(newNote);
+  
+    notes.push(newNote);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), err => {
+        if (err) throw err;
+    });
+  
+    res.json(newNote);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes/notesApiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes/notesHtmlRoutes.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
+
+
+// app.delete('api/notes/:id', (req, res) => {
+//     console.log(req.params);
+//     const deletedNote = (req.params.id);
+// })
