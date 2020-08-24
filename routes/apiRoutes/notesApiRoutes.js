d0836a4badc0ff3dadb1817d74ffb97879e3d4d8
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-08-24 01:05:13 -0500

    added screenshot

--- routes/apiRoutes/notesApiRoutes.js
+++ routes/apiRoutes/notesApiRoutes.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const uniqid = require('uniqid');
 const router = require('express').Router();
 const notes = require('../../db/db.json');
+const path = require('path');
 
 router.get('/notes', (req, res) => {
     let data = notes;
@@ -11,39 +12,87 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uniqid();
-    console.log(newNote);
+    console.log("This is the newNote" + newNote);
     notes.push(newNote);
 
     fs.writeFile('db/db.json', JSON.stringify(notes), err => {
         if (err) throw err;
     });
     res.json(newNote);
+
+    /****
+     *  const newNote = req.body;
+    newNote.id = uniqid();
+    console.log("This is a new NOte" + newNote);
+    
+
+    fs.readFile("../../db/db.json", (err, data) => {
+        if (err) throw err;
+        let dbFile = JSON.parse(data);
+        dbFile.push(newNote);
+
+        fs.writeFile('../../db/db.json', JSON.stringify(dbFile), "utf8", err => {
+            if (err) throw err;
+            console.log("data was saved")
+        });
+
+
+    })
+     * 
+     */
 });
 
 
-// wor in progress for delete button
-// router.delete('notes/:id', (req, res) => {
-//     const deletedNote = (req.params.id);
+// work in progress for delete button
+// router.delete('notes/:id', function (req, res) {
 
-//     console.log(deletedNote);
 
-//     notes = notes.filter(function (obj) {
-//         return obj.id !== deletedNote;
-//     });
 
-//     for (var i = 0; i < notes.length; i++) {
-//         if (notes.indexOf(deletedNote) !== -1) {
-//             notes.splice(i, 1);
-//         }
-//     };
+//      const db = fs.readFileSync(path.join(__dirname, "/db/db.json"));
 
-//     fs.writeFile('db/db.json', JSON.stringify(notes), function (err, data) {
-//         if (err) {
-//             throw err 
-//         }
+//      const dbFile = JSON.parse(db);
+//      console.log("this is the dbFile" + dbFile);
+
+//      const deletedNote = req.params.id;
+
+//      for (let x = 0; x < dbFile.length; x++) {
+
+//          if(dbFile[x].id.toString() === deletedNote) {
+//        dbFile.splice(x, 1);
+//             console.log(dbFile, "deleted")
+//            break;
+//          }
+//      }
+    
+//      fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(dbFile));
+
+//      res.sendStatus(200);
+
+
+    
+    /*
+    const deletedNote = (req.params.id);
+
+    console.log(deletedNote);
+
+    notes = notes.filter(function (obj) {
+        return obj.id !== deletedNote;
+    });
+
+    for (var i = 0; i < notes.length; i++) {
+        if (notes.indexOf(deletedNote) !== -1) {
+            notes.splice(i, 1);
+        }
+    };
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), function (err, data) {
+        if (err) {
+            throw err 
+        }
         
-//         res.send(data);
-//     });    
+        res.send(data);
+    });
+    */    
 // });
 
 module.exports = router;
--- routes/htmlRoutes/notesHtmlRoutes.js
+++ routes/htmlRoutes/notesHtmlRoutes.js
@@ -9,4 +9,5 @@ router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
+
 module.exports = router;
